918484ddf9274b9a4641325bdc6a9945fd10ab5f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-23 21:34:16 +0530

    Drew an energyBar filler

--- src/bookshelf.lua
+++ src/bookshelf.lua
@@ -3,21 +3,21 @@ local BOOKSHELF = {}
 local rectShelves = {
     {
         w = 400,
-        h = wH/1.5,
-        x = 600 ,
+        h = wH / 1.5,
+        x = 600,
         y = wH - 200,
         rot = 9.5
     },
     {
         w = 400,
-        h = wH/1.5,
-        x = wW-200,
+        h = wH / 1.5,
+        x = wW - 200,
         y = wH - 200
     },
 }
 
 local hoverTarget = {
-    x = wW/2 - 30,
+    x = wW / 2 - 30,
     w = 60,
 
     y = 0,
@@ -27,20 +27,18 @@ local hoverTarget = {
 }
 
 local clickCircles = {}
-
+local mx, my = 0, 0
 function BOOKSHELF:load()
     energyBar = {
         max = 20,
         fill = 0,
         rate = 2,
-        decr = 4,
-        decrT = 1
+        decr = 0.15,
     }
-
 end
 
 function BOOKSHELF:update(dt)
-    local mx, my = love.mouse.getPosition()
+    mx, my = love.mouse.getPosition()
 
     if mx > hoverTarget.x and mx < hoverTarget.x + hoverTarget.w and my > hoverTarget.y and my < hoverTarget.y + hoverTarget.h then
         hoverTarget.hovering = true
@@ -49,30 +47,38 @@ function BOOKSHELF:update(dt)
     end
 
     for i, v in ipairs(clickCircles) do
-        
         v.t = math.max(0, v.t - dt)
-        v.rad = v.t/1 * v.rad
+        v.rad = v.t / 1 * v.rad
         if v.t <= 0 then table.remove(clickCircles, i) end
-    
     end
 end
 
 function BOOKSHELF:draw()
-
     local rS = rectShelves
-    lg.setColor(1,0,1)
+    lg.setColor(1, 0, 1)
     drawRectangle("fill", rS[1].x, rS[1].y, rS[1].w, rS[1].h, rS[1].w, rS[1].h, rS[1].rot)
-    lg.setColor(1,1,0)    
+    lg.setColor(1, 1, 0)
     drawRectangle("fill", rS[2].x, rS[2].y, rS[2].w, rS[2].h, rS[2].w, rS[2].h, 0)
 
-    lg.setColor(1,1,1)
+    lg.setColor(1, 1, 1)
     for i, v in ipairs(clickCircles) do
         if v.rad >= 1 then
-                    lg.circle("line",v.x, v.y, v.rad)
-
+            lg.circle("line", v.x, v.y, v.rad)
         end
     end
+    if energyBar.fill ~= 0 and energyBar.fill < energyBar.max then
+        energyBar.fill = math.max(0, energyBar.fill - energyBar.decr)
+    end
 
+    if hoverTarget.hovering then
+        lg.setFont(fontM)
+        local text = "Move Shelf" 
+        lg.print(text, mx - fontM:getWidth(text)/2, my + 20)
+        lg.rectangle("fill", mx - fontM:getWidth(text)/2, my + 20 + fontM:getHeight(), fontM:getWidth(text), 20)
+        lg.setColor(0,0,1)
+        lg.rectangle("fill", mx - fontM:getWidth(text)/2, my + 20 + fontM:getHeight(), fontM:getWidth(text) * energyBar.fill/energyBar.max, 20)
+    end
+    
 end
 
 function BOOKSHELF:mousepressed(x, y, button)
