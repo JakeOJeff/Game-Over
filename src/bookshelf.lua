7b1cbd2a84558dc41520bc5ad9d08653010068dc
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-23 20:49:36 +0530

    Working toppled shelf

--- src/bookshelf.lua
+++ src/bookshelf.lua
@@ -1,7 +1,23 @@
 local BOOKSHELF = {}
 
-function BOOKSHELF:load()
+local rectShelves = {
+    {
+        w = 400,
+        h = wH/1.5,
+        x = 600 ,
+        y = wH - 200,
+        rot = 9.5
+    },
+    {
+        w = 400,
+        h = wH/1.5,
+        x = wW-200,
+        y = wH - 200
+    },
+}
 
+function BOOKSHELF:load()
+    
 end
 
 function BOOKSHELF:update(dt)
@@ -9,6 +25,20 @@ end
 
 function BOOKSHELF:draw()
 
+    local rS = rectShelves
+    lg.setColor(1,0,1)
+    drawRectangle("fill", rS[1].x, rS[1].y, rS[1].w, rS[1].h, rS[1].w, rS[1].h, rS[1].rot)
+    lg.setColor(1,1,0)    
+    drawRectangle("fill", rS[2].x, rS[2].y, rS[2].w, rS[2].h, rS[2].w, rS[2].h, 0)
+
+end
+
+function drawRectangle(mode, x, y, width, height, ox, oy, angle)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    love.graphics.rotate(math.rad(angle))
+    love.graphics.rectangle(mode, -ox, -oy, width, height)
+    love.graphics.pop()
 end
 
 return BOOKSHELF
